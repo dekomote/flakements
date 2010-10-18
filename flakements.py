b0157638628f22ebfb2a686186fb91c677c18cdc
Dejan Noveski <dekomote@gmail.com>
2010-10-18 02:28:43 +0200

    Added pep8 error parser. Removed lite terminal output, full output supports trimming. Errors can be printed on terminal without the code. Different colors for pep8 and pyflakes errors. Lines that overlap a pep8 and a pyflakes error, colored in third color.

--- flakements.py
+++ flakements.py
@@ -10,7 +10,13 @@ from pygments.lexers import PythonLexer
 from pygments.formatters import HtmlFormatter, TerminalFormatter
 from termcolor import colored
 import sys, os, codecs
+import uuid
+
 sys.stdout = codecs.getwriter('utf8')(sys.stdout)
+
+FULL_TRIM = 2
+SMALL_TRIM = 1
+NO_TRIM = 0
 
 def check_source(code_string, filename = "<string>"):
     """Checker function to be used in code"""
@@ -46,6 +52,25 @@ def check_source(code_string, filename = "<string>"):
                     } for warning in w.messages]
 
 
+def pep8_source(code_string):
+    """docstring for pep8_source"""
+    
+    temp_filename = uuid.uuid4().hex + ".py"
+    temp_file = open(temp_filename, "wb").write(codecs.BOM_UTF8 + code_string.encode("utf-8"))
+    pep8_output = os.popen("pep8 %s"%temp_filename).read()
+    os.remove(temp_filename)
+    return _parse_pep8(pep8_output)
+
+
+def _parse_pep8(pep8_output):
+    """docstring for _parse_pep8"""
+    if not pep8_output:
+        return []
+    
+    pep8_lines = pep8_output.splitlines(1)
+    return [{"line": int(pl.split(":")[1]), "offset": int(pl.split(":")[2]), "message": pl.split(":")[3].rstrip("\n")} for pl in pep8_lines]
+        
+
 
 class Flakement(object):
     """The main Flakement class."""
@@ -53,6 +78,7 @@ class Flakement(object):
         
         self.code_string, self.filename = code_string, filename
         self.code_errors = check_source(code_string, filename)
+        self.pep_errors = pep8_source(code_string)
 
     def _enumerated_code_lines(self):
         code_lines = self.code_string.splitlines(1)
@@ -62,71 +88,56 @@ class Flakement(object):
 
         return code_lines
 
-    def _adjusted_line_padding(self, line, line_pad):
-        code_lines = self._enumerated_code_lines()
-
-        line_pad_up = line - line_pad
-        line_pad_down = line + line_pad
-        
-        if line_pad_up < 0: line_pad_up = 0
-        if line_pad_down > len(code_lines): line_pad_down = len(code_lines)
-
-        return line_pad_up, line_pad_down
-
     
-    def terminal_output(self, line_pad = 3):
+    def terminal_full_output(self, trim_output = NO_TRIM):
         
         if not self.code_errors == []:
+        
+            print (colored("File: %s"%(self.filename),
+                    "cyan"))
+            code_lines = self._enumerated_code_lines()
+            code_error_lines =  [ex["line"] for ex in self.code_errors]
+            pep_error_lines =  [ex["line"] for ex in self.pep_errors]
+            error_lines = code_error_lines + pep_error_lines 
             
-            print (colored("File: %s"%(self.filename),
-                    "white", "on_red"))
-            code_lines = self._enumerated_code_lines()
-        
-            for ex in self.code_errors:
-                line = ex["line"]
-                message = ex["message"]
-                offset = ex["offset"]
-                        
-                line_pad_up, line_pad_down = \
-                    self._adjusted_line_padding(line, line_pad)
+            if trim_output == SMALL_TRIM:
+                low_line = min(error_lines)
+                high_line = max(error_lines)
+            else:
+                low_line = 0
+                high_line = len(code_lines)
 
-                sys.stdout.write(highlight(
-                    "".join(code_lines[line_pad_up:line-1]),
-                    PythonLexer(), TerminalFormatter()))
-                print (colored(code_lines[line-1], "white", "on_red"))
-                sys.stdout.write(highlight(
-                    "".join(code_lines[line:line_pad_down]),
-                    PythonLexer(), TerminalFormatter()))
-                print "\n"
-                print (colored("L-%s:C-%s %s"%(line, offset, message),
-                    "white", "on_red"))
-                print "\n\n"
+            for ln,cl in enumerate(code_lines): 
+                if ln >= low_line and ln <= high_line:
+                    if ln + 1 not in error_lines:
+                        if not trim_output == FULL_TRIM:
+                            sys.stdout.write(highlight(cl, PythonLexer(),
+                                TerminalFormatter()))
+                    else:
+                        if ln + 1 in code_error_lines and ln + 1 in pep_error_lines:
+                            print colored(cl.rstrip("\n"), "white", "on_magenta")
+                        elif ln + 1 in code_error_lines:
+                            print colored(cl.rstrip("\n"), "white", "on_red")
+                        else:
+                            print colored(cl.rstrip("\n"), "white", "on_blue")
 
-    
-    def terminal_full_output(self):
-        
-        if not self.code_errors == []:
-        
-            print (colored("File: %s"%(self.filename),
-                    "white", "on_red"))
-            code_lines = self._enumerated_code_lines()
-        
-            error_lines = [ex["line"] for ex in self.code_errors]
-
-            for ln,cl in enumerate(code_lines):
-                if ln + 1 not in error_lines:
-                    sys.stdout.write(highlight(cl, PythonLexer(),
-                        TerminalFormatter()))
-                else:
-                    print colored (cl, "white", "on_red")
             print "\n"
-            for ex in self.code_errors:
-                line = ex["line"]
-                offset = ex["offset"]
-                message = ex["message"]
-                print (colored("L-%s:C-%s %s"%(line, offset, message),
-                    "white", "on_red"))
-            print "\n\n"
+            self.print_errors_terminal()
+
+    def print_errors_terminal(self):
+        for ex in self.code_errors:
+            line = ex["line"]
+            offset = ex["offset"]
+            message = ex["message"]
+            print (colored("L-%s:C-%s %s"%(line, offset, message),
+                "white", "on_red"))
+        for ex in self.pep_errors:
+            line = ex["line"]
+            offset = ex["offset"]
+            message = ex["message"]
+            print (colored("L-%s:C-%s %s"%(line, offset, message),
+                 "white", "on_blue"))
+        print "\n\n" 
 
     
     def html_output(self, linenos = "table", hl_errors = True,
@@ -161,4 +172,4 @@ def main():
     raise SystemExit(0)
 
 if __name__ == "__main__":
-    main()
+    main() 
