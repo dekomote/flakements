4335ab472f97123925eda7ef34375aa04dc219ca
Dejan Noveski <dn@atomidata.com>
2010-10-18 11:29:06 +0200

    docstrings added. need enhancement.

--- flakements.py
+++ flakements.py
@@ -1,5 +1,11 @@
 """
-Pyflakes checker to be used with API
+Flakements wraps pyflakes and pep8, enchancing their output
+with pygments. Can be used for output of code/errors or just
+for in-source code checking. The helper functions check_source
+and pep8_source, provide easy interface to pep8+pyflakes
+and unify the output of those tools.
+
+USAGE flakements.py filename
 """
 
 import compiler
@@ -12,6 +18,7 @@ from termcolor import colored
 import sys, os, codecs
 import uuid
 
+#we need to make stdout utf-8
 sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 
 FULL_TRIM = 2
@@ -19,9 +26,14 @@ SMALL_TRIM = 1
 NO_TRIM = 0
 
 def check_source(code_string, filename = "<string>"):
-    """Checker function to be used in code"""
+    """Checking code string with pyflakes.
+        filename is optional as it is needed by
+        the compiler.
+        
+        returns list"""
     
     try:
+        #compile the code and check for syntax errors
         compile(code_string, filename, "exec")
     except SyntaxError, value:
         msg = value.args[0]
@@ -43,9 +55,11 @@ def check_source(code_string, filename = "<string>"):
             return [{"line": lineno, "offset": offset, "message": msg}]
 
     else:
+        #no syntax errors, check it with pyflakes
         tree = compiler.parse(code_string)
         w = checker.Checker(tree, filename)
         w.messages.sort(lambda a, b: cmp(a.lineno, b.lineno))
+
         return [{"line": warning.__dict__.get("lineno", 0), 
                         "offset": warning.__dict__.get("offset", 0),
                         "message": warning.message % warning.message_args
@@ -53,27 +67,42 @@ def check_source(code_string, filename = "<string>"):
 
 
 def pep8_source(code_string):
-    """docstring for pep8_source"""
+    """Check code string with pep8 script.
+        
+        returns list"""
     
     temp_filename = uuid.uuid4().hex + ".py"
-    temp_file = open(temp_filename, "wb").write(codecs.BOM_UTF8 + code_string.encode("utf-8"))
+    temp_file = open(temp_filename, "wb").write(
+            codecs.BOM_UTF8 + code_string.encode("utf-8"))
     pep8_output = os.popen("pep8 %s"%temp_filename).read()
     os.remove(temp_filename)
     return _parse_pep8(pep8_output)
 
 
 def _parse_pep8(pep8_output):
-    """docstring for _parse_pep8"""
+    """pep8 script doesn't provide api so I had to
+        hack through this one.
+        
+        returns list"""
+
     if not pep8_output:
         return []
     
     pep8_lines = pep8_output.splitlines(1)
-    return [{"line": int(pl.split(":")[1]), "offset": int(pl.split(":")[2]), "message": pl.split(":")[3].rstrip("\n")} for pl in pep8_lines]
+    return [{"line": int(pl.split(":")[1]), 
+            "offset": int(pl.split(":")[2]), 
+            "message": pl.split(":")[3].rstrip("\n")} for pl in pep8_lines]
         
 
 
 class Flakement(object):
-    """The main Flakement class."""
+    """The main Flakement class. When instanciated, the class
+        has the code checked and keeps 2 lists with errors, one
+        with pep8 errors and 1 with pyflakes errors.
+        
+        Provides methods for output on terminal/html.
+        Uses pygments"""
+
     def __init__(self, code_string, filename):
         
         self.code_string, self.filename = code_string, filename
@@ -81,6 +110,8 @@ class Flakement(object):
         self.pep_errors = pep8_source(code_string)
 
     def _enumerated_code_lines(self):
+        """Returns a list of enumerated, formated code lines"""
+
         code_lines = self.code_string.splitlines(1)
 
         for k,v in enumerate(code_lines):
@@ -90,7 +121,13 @@ class Flakement(object):
 
     
     def terminal_full_output(self, trim_output = NO_TRIM):
-        
+        """Prints the code and errors in terminal.
+            Output can be trimmed to show only the error lines + errors -
+            FULL_TRIM, show from first to last line with error - SMALL_TRIM,
+            or NO_TRIM- shows the whole code. PEP8 and pyflakes error lines
+            have different colors. If two different errors happen on same
+            line, the line gets third color"""
+
         if not self.code_errors == []:
         
             print (colored("File: %s"%(self.filename),
@@ -114,17 +151,25 @@ class Flakement(object):
                             sys.stdout.write(highlight(cl, PythonLexer(),
                                 TerminalFormatter()))
                     else:
-                        if ln + 1 in code_error_lines and ln + 1 in pep_error_lines:
-                            print colored(cl.rstrip("\n"), "white", "on_magenta")
+                        if ln + 1 in code_error_lines and \
+                                ln + 1 in pep_error_lines:
+                            print colored(cl.rstrip("\n"), 
+                                    "white", "on_magenta")
                         elif ln + 1 in code_error_lines:
-                            print colored(cl.rstrip("\n"), "white", "on_red")
+                            print colored(cl.rstrip("\n"), 
+                                    "white", "on_red")
                         else:
-                            print colored(cl.rstrip("\n"), "white", "on_blue")
+                            print colored(cl.rstrip("\n"), 
+                                    "white", "on_blue")
 
             print "\n"
             self.print_errors_terminal()
 
     def print_errors_terminal(self):
+        """Prints the errors in terminal
+            PEP8 and pyflakes errors get different
+            colors."""
+
         for ex in self.code_errors:
             line = ex["line"]
             offset = ex["offset"]
@@ -142,14 +187,20 @@ class Flakement(object):
     
     def html_output(self, linenos = "table", hl_errors = True,
             lineanchors = "code-line"):
-        """HTML Output for the Flakements class"""
+        """Returns a tuple of HTML output of the code and errors
+            args affect the HtmlFormatter."""
+
         if hl_errors:
-            hl_lines = [ex["line"] for ex in self.code_errors + self.pep_errors]
+            hl_lines = [ex["line"] 
+                    for ex in self.code_errors + self.pep_errors]
         return highlight(self.code_string, PythonLexer(), 
                 HtmlFormatter(linenos = linenos, hl_lines = hl_lines, 
-                    lineanchors = lineanchors)), self.code_errors, self.pep_errors
+                    lineanchors = lineanchors)), \
+                            self.code_errors, self.pep_errors
         
 def main():
+    """Temporary main method"""
+
     args = sys.argv[1:]
     if args:
         for arg in args:
